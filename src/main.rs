b48e3d6644c0b515a35045404490bd1cb3c4442b
Michael Thoresen <mimothor@hotmail.com>
2023-01-10 21:10:18 +0100

    trying to make reading db work, with limited success

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,13 @@ use std::time::SystemTime;
 use dotenv::dotenv;
 
 use bonsaidb::{
-    core::schema::{Collection, SerializedCollection},
+    core::{
+        document::{CollectionDocument, Emit},
+        schema::{
+            view::CollectionViewSchema, Collection, ReduceResult, SerializedCollection,
+            SerializedView, View, ViewMapResult, ViewMappedValue,
+        },
+    },
     local::{
         config::{Builder, StorageConfiguration},
         Database,
@@ -23,12 +29,36 @@ use serenity::{
 use serde::{Deserialize, Serialize};
 
 #[derive(Debug, Serialize, Deserialize, Collection)]
-#[collection(name = "tickets")]
+#[collection(name = "tickets", views = [ListOfTickets])]
 struct Ticket {
+    pub id: u32,
     pub timestamp: SystemTime,
     pub user: i32,
     pub message: String,
     pub status: String,
+}
+
+#[derive(Debug, Clone, View)]
+#[view(collection = Ticket, key = u32, value = usize, name = "list-of-tickets")]
+struct ListOfTickets;
+
+impl CollectionViewSchema for ListOfTickets {
+    type View = Self;
+
+    fn map(&self, document: CollectionDocument<Ticket>) -> ViewMapResult<Self::View> {
+        document
+            .header
+            .emit_key_and_value(document.contents.id, 1)
+    }
+
+    fn reduce(
+        &self,
+        mappings: &[ViewMappedValue<Self>],
+        _rereduce: bool,
+    ) -> ReduceResult<Self::View> {
+        Ok(mappings.iter().map(|m| m.value).sum())
+    }
+    
 }
 
 #[group]
@@ -48,6 +78,7 @@ async fn main() -> Result<(), bonsaidb::core::Error> {
     let db = Database::open::<Ticket>(StorageConfiguration::new("ticket.storage"))?;
 
     let ticket = Ticket {
+        id: 1,
         timestamp: SystemTime::now(),
         user: 123,
         message: String::from("Test ticket"),
@@ -88,6 +119,9 @@ async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
 
 #[command]
 async fn list(ctx: &Context, msg: &Message) -> CommandResult {
+    
+    let tickets = ListOfTickets::entries(&db).with_key(&1).query()?;
+    println!("Number of tickets: {}", tickets.len());
     msg.reply(ctx, "Listing Tickets...").await?;
 
     Ok(())
